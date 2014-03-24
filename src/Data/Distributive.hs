ec521783331a5b5508f8c9f2b94ed7bc978d5ac0
Edward Kmett <ekmett@gmail.com>
2014-03-24 13:47:35 -0400

    update copyright

--- src/Data/Distributive.hs
+++ src/Data/Distributive.hs
@@ -2,7 +2,7 @@
 -----------------------------------------------------------------------------
 -- |
 -- Module      :  Data.Distributive
--- Copyright   :  (C) 2011-2013 Edward Kmett
+-- Copyright   :  (C) 2011-2014 Edward Kmett
 -- License     :  BSD-style (see the file LICENSE)
 --
 -- Maintainer  :  Edward Kmett <ekmett@gmail.com>
