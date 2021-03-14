ce3ab06bfe46c924bc2b335cbea03471b37601d0
Edward Kmett <ekmett@gmail.com>
2021-03-14 12:00:22 -0700

    HKD Distributive Endo

---
+++ src/Data/HKD/Distributive/Endo.hs
@@ -0,0 +1,48 @@
+{-# Language CPP #-}
+{-# Language Safe #-}
+{-# Language PatternSynonyms #-}
+{-# Language ViewPatterns #-}
+{-# Language TypeOperators #-}
+{-# Language RankNTypes #-}
+-- |
+-- Copyright   : (C) 2021 Edward Kmett
+-- License     : BSD-style (see the file LICENSE)
+--
+-- Maintainer  : Edward Kmett <ekmett@gmail.com>
+-- Stability   : provisional
+-- Portability : non-portable (ghc 8.0+)
+--
+-- Tabulated endomorphisms
+module Data.HKD.Distributive.Endo 
+( FEndo(.., FEndo, appFEndo)
+) where
+
+import Data.HKD
+import Data.HKD.Distributive
+
+#if __GLASGOW_HASKELL__ < 804
+import Data.Semigroup (Semigroup(..))
+#endif
+
+-- | Tabulated endomorphisms.
+--
+-- Many representable functors can be used to memoize functions.
+newtype FEndo f = FEndoDist { runFEndoDist :: f (FLog f) }
+
+pattern FEndo :: FDistributive f => (FLog f ~> FLog f) -> FEndo f
+pattern FEndo { appFEndo } = FEndoDist (FTabulate appFEndo)
+
+{-# complete FEndo #-}
+
+instance FDistributive f => Semigroup (FEndo f) where
+  FEndo f <> FEndo g = FEndo (f . g)
+  {-# inline (<>) #-}
+
+instance FDistributive f => Monoid (FEndo f) where
+#if __GLASGOW_HASKELL__ < 804
+  FEndo f `mappend` FEndo g = FEndo (f . g)
+  {-# inline mappend #-}
+#endif
+  mempty = FEndoDist faskDist
+  {-# inline mempty #-}
+
