761dbb7a50f1583f6fa58fda591bb88796370810
Aaron Vargo <fpfundamentalist@gmail.com>
2021-03-15 18:10:52 -0700

    add HKD

--- src/Data/HKD/Distributive.hs
+++ src/Data/HKD/Distributive.hs
@@ -8,6 +8,7 @@
 {-# Language ExistentialQuantification #-}
 {-# Language FlexibleContexts #-}
 {-# Language FlexibleInstances #-}
+{-# Language GADTs #-}
 {-# Language GeneralizedNewtypeDeriving #-}
 {-# Language KindSignatures #-}
 {-# Language LambdaCase #-}
@@ -513,6 +514,57 @@ flogFromFLogarithm = \(FLogarithm f) -> f faskFDist
 flogToFLogarithm :: FDistributive f => FLog f ~> FLogarithm f
 flogToFLogarithm = \f -> FLogarithm (ftraceFDist f)
 {-# inline flogToFLogarithm #-}
+
+-------------------------------------------------------------------------------
+-- HKD
+-------------------------------------------------------------------------------
+
+newtype HKD (f :: Type -> Type) (a :: () -> Type) = HKD { runHKD :: f (a '()) }
+
+mapHKD :: (f (a '()) -> g (b '())) -> HKD f a -> HKD g b
+mapHKD = \f -> HKD #. f .# runHKD
+{-# inline mapHKD #-}
+
+newtype DHKD w f = DHKD { runDHKD :: w (HKD f) }
+
+instance FFunctor w => FFunctor (DHKD w) where
+  ffmap f = DHKD #. ffmap (mapHKD f) .# runDHKD
+  {-# inline ffmap #-}
+
+instance Functor f => FFunctor (HKD f) where
+  ffmap = \f -> mapHKD (fmap f)
+  {-# inline ffmap #-}
+
+instance Contravariant f => FContravariant (HKD f) where
+  fcontramap = \f -> HKD #. contramap f .# runHKD
+  {-# inline fcontramap #-}
+
+instance Foldable f => FFoldable (HKD f) where
+  ffoldMap = \f -> foldMap f .# runHKD
+  {-# inline ffoldMap #-}
+
+instance Traversable f => FTraversable (HKD f) where
+  ftraverse = \f -> fmap HKD . traverse f .# runHKD
+  {-# inline ftraverse #-}
+
+instance Applicative f => FZip (HKD f) where
+  fzipWith = \f (HKD fab) (HKD fa) -> HKD (liftA2 f fab fa)
+  {-# inline fzipWith #-}
+instance Applicative f => FRepeat (HKD f) where
+  frepeat = HKD #. pure
+  {-# inline frepeat #-}
+
+data HKDFLog f (a :: ()) where
+  HKDFLog :: Log f -> HKDFLog f '()
+
+instance Distributive f => FDistributive (HKD f) where
+  type FLog (HKD f) = HKDFLog f
+  fscatter = \k g w -> HKD $ distrib (DHKD (ffmap g w)) $ k . ffmap coerce .# runDHKD
+  {-# inline fscatter #-}
+  findex = \(HKD fa) (HKDFLog lg) -> index fa lg
+  {-# inline findex #-}
+  ftabulate = \f -> HKD $ tabulate (f . HKDFLog)
+  {-# inline ftabulate #-}
 
 -------------------------------------------------------------------------------
 -- LKD
